8db2bf3b94aef2e37db42c0917fcf491179c0a68
Greg <gregory.milet@gmail.com>
2024-07-07 20:50:26 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Custom KASM images',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.io/GM-Ant/kasmimages/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasmimages/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
